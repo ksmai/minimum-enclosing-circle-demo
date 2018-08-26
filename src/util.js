0645a6f996c56a7f77302e18c0c416a08e7c805e
ksmai <kasingmai@gmail.com>
2018-08-26 19:54:56 +0800

    Add util functions to calculate lines

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import Phaser from 'phaser';
+
 export function toIntColor(color) {
   if (typeof color === 'number') {
     return color;
@@ -12,3 +14,27 @@ export function toStringColor(color) {
   return '#' + color.toString(16).padStart(6, '0');
 }
 
+export function midpoint(p, q) {
+  const line = new Phaser.Geom.Line(p.x, p.y, q.x, q.y);
+  return Phaser.Geom.Line.GetMidPoint(line);
+}
+
+export function distance(p, q) {
+  const dx = p.x - q.x;
+  const dy = p.y - q.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+export function bisector(p, q) {
+  const line = new Phaser.Geom.Line(p.x, p.y, q.x, q.y);
+  const slope = Phaser.Geom.Line.Slope(line);
+  const midpoint = Phaser.Geom.Line.GetMidPoint(line);
+  const newSlope = -1 / slope;
+  if (newSlope === 0) {
+    return new Phaser.Geom.Line(0, midpoint.y, 65536, midpoint.y);
+  } else if (isFinite(newSlope)) {
+    return new Phaser.Geom.Line(0, midpoint.y - midpoint.x * newSlope, 65536, midpoint.y + (65536 - midpoint.x) * newSlope);
+  } else {
+    return new Phaser.Geom.Line(midpoint.x, 0, midpoint.x, 65536);
+  }
+}
